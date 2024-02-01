f8b0c097f34a97447d46f5cec7fa683432dd741a
anibe <anibe@ANI-DESKTOP>
2024-02-01 19:00:45 -0400

    Add project files.

---
+++ Programa 3 - 3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class CDigitos
+{
+    static void Main(string[] args)
+    {
+        int numero = Numero("Ingrese un número positivo: ");
+
+        if (numero >= 1 && numero <= 9)
+        {
+            Console.WriteLine("El número tiene un dígito.");
+        }
+        else if (numero >= 10 && numero <= 99)
+        {
+            Console.WriteLine("El número tiene dos dígitos.");
+        }
+        else
+        {
+            Console.WriteLine("El número no es válido.");
+        }
+    }
+
+    static int Numero(string mensaje)
+    {
+        int num;
+        bool esValido;
+
+        do
+        {
+            Console.WriteLine(mensaje);
+            esValido = int.TryParse(Console.ReadLine(), out num);
+
+            if (!esValido)
+            {
+                Console.WriteLine("\n" + "El valor ingresado no es válido." + "\n" + "Por favor, ingrese un número." + "\n");
+            }
+        } while (!esValido);
+
+        return num;
+    }
+}
